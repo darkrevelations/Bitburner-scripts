258d09d9e6cb4f776c5239cf243095171fa26b32
DarkRevelations <52640034+darkrevelations@users.noreply.github.com>
2024-11-11 19:01:08 -0600

    Create manifest.js
    Added manifest for updater (in progress)

---
+++ apps/odin/manifest.js
@@ -0,0 +1,17 @@
+export const File_Manifest = {
+  "updater.js":{
+    "location":"apps/odin/system/updater.js",
+    "version":"0.0.1",
+    "githubURL":""
+  },
+  "odin.js":{
+    "location":"apps/odin/odin.js",
+    "version":"0.0.1",
+    "githubURL":""
+  },
+  "mainConfig.js":{
+    "location":"apps/odin/system/configs/mainConfig.js",
+    "version":"0.0.1",
+    "githubURL":""
+  }
+}
